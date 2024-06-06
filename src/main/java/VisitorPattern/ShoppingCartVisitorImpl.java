b5d5aa0483a4ad2875ba57f889dd5c5548964e70
heisenbuggs <prasukjain0047@gmail.com>
2024-06-06 09:49:44 +0530

    Minor code refactoring

--- src/main/java/VisitorPattern/ShoppingCartVisitorImpl.java
+++ src/main/java/VisitorPattern/ShoppingCartVisitorImpl.java
@@ -5,8 +5,8 @@ public class ShoppingCartVisitorImpl implements ShoppingCartVisitor {
     @Override
     public int visit(Book book) {
         int cost;
-        //apply 5$ discount if book price is greater than 50
-        if(book.getPrice() > 50) {
+        // apply 5$ discount if book price is greater than 50
+        if (book.getPrice() > 50) {
             cost = book.getPrice()-5;
         } else {
             cost = book.getPrice();
